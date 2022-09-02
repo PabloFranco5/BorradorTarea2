86b4712ef7bd75e79da282d56af65cd6f905156b
Juan Pablo Montoya <juanpismontollaxd@gmail.com>
2022-09-02 00:29:52 -0500

    desarrollo basico completo

--- main.py
+++ main.py
@@ -16,23 +16,34 @@ def execute(args):
 
 
     while True:
-        # Para el registro de la historia clinica vamos a pedirle 
-        # Nombre, apellidos, Identificacion, Numero de contacto, Nombre 
-        # de la EPS a la que esta afiliado
+        # Para el registro de la historia clinica vamos a pedirle:
+
         user_input = input("Ingrese los datos del paciente: ")
 
-        nombre, apellidos, identificacion, Eps, contacto, sangre, edad =user_input.split(" ")
+        nombre, apellidos, identificacion, Eps, contacto, sangre, edad, direccion, antecedentes, problema, medico, documento, observaciones, cuidados, evolucion, responsable, quirurgica, alta  =user_input.split(" ")
 
         #vamos a hacer el metodo de create
         Paciente_id = database.create( {
-            "nombre" : nombre,
-            "apellidos" : apellidos,
-            "identificacion" : identificacion,
-            "Eps": Eps,
-            "numero_de_contacto" : contacto,
-            "tipo_de_sangre" :sangre,
-            "edad": edad
+            "nombre_del_paciente" : str(nombre),
+            "apellidos_del_paciente" : str(apellidos),
+            "identificacion_del_paciente" : int(identificacion),
+            "Eps_que_lo_cubre": int(Eps),
+            "numero_de_contacto" : int(contacto),
+            "tipo_de_sangre" : sangre,
+            "edad_": int(edad),
+            "direccion_de_residencia": str(direccion),
+            "antecedentes_de_interes_del_paciente": str(antecedentes),
+            "descripcion_del_problema": str(problema), 
+            "nombre_del_medico_que_lo_atendio":str(medico),
+            "identificacion_profesional": int(documento),
+            "observaciones": str(observaciones),
+            "planificacion_de_cuidados": str(cuidados),
+            "evolucion_del_paciente": str(evolucion),
+            "adulto_responsable_en_caso_de_emergencia":str(responsable),
+            "informacion_quirurgica": str(quirurgica),
+            "informacion_de_alta": str(alta)
             })
+
 
         print(database.read(Paciente_id))
 
